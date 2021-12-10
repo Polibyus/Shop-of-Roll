1776150ec2e879338c2ccea5fd98538eb76044f6
Polibyus <89615619+Polibyus@users.noreply.github.com>
2021-12-10 17:00:14 -0300

    Prueba de BS Icons en cart

---
+++ src/components/CartIcon.js
@@ -0,0 +1,12 @@
+import { FaShoppingCart } from 'react-icons/fa';
+import React from 'react'
+
+const CartIcon = () => {
+    return (
+        <div>
+            <a href> FaShoppingCart </a>
+        </div>
+    )
+}
+
+export default CartIcon
