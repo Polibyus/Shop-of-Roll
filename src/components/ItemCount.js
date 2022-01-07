83ad05e6f6e58359a6514aae8e3b2ae6ebaf3dba
Polibyus <89615619+Polibyus@users.noreply.github.com>
2022-01-07 15:17:03 -0300

    Actualizado el counter para tener un boton de confirmar y cumplir el desafio

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,13 +4,14 @@ const ItemCount = (props) => {
     const [venta, setVenta] = useState(1);
     const [menos, setMenos] = useState(true);
     const [mas, setMas] = useState(false);
+    const [confirma, setConfirma] = useState(false);
     const stocked = (props.stock > 0) ? true : false;
     useEffect(() => {
         if (venta === props.stock) {setMas(true);}
             else setMas(false);
         if (venta > 1) {setMenos(false);}
             else setMenos(true);
-    });
+    }, [venta, setMas, setMenos]);
 
     const sumar = () => {
         if (venta < props.stock)
@@ -24,24 +25,28 @@ const ItemCount = (props) => {
     return (
         <>
         {stocked
+        ?
+            confirma
             ?
-            <>
-            <div className="counter">
-                <button disabled={menos} onClick={restar}>-</button>
-                <h4>{venta}</h4>
-                <button disabled={mas} onClick={sumar}>+</button>
-            </div>
-                <div className="col-lg-12 mt-3">
+            <div className="col-lg-12 mt-3">
                 <div className="row">
                     <div className="col-lg-6 pb-2">
-                        <a href="/carrito" className="btn btn-danger w-100">Al carrito!</a>
+                        <a href="/carrito" className="btn btn-danger w-100">Al carrito</a>
                     </div>
                     <div className="col-lg-6 pb-2">
-                        <a href="/finish" className="btn btn-success w-100">Comprar ahora</a>
+                        <a href="/" className="btn btn-success w-100">Comprar más</a>
                     </div>
                 </div>
             </div>
-            </>
+            :
+            <div>
+                <div className="counter">
+                    <button className="btn btn-danger" disabled={menos} onClick={restar}>-</button>
+                    <h4>{venta}</h4>
+                    <button className="btn btn-success" disabled={mas} onClick={sumar}>+</button>
+                    <button className="btn btn-primary" onClick={() => setConfirma(true)}>Confirmar</button>
+                </div>
+            </div>
             :
             <h2>AGOTADO</h2>
         }
