a50406dfb97cd14612e3e4ab569375eaeb097f1a
Polibyus <89615619+Polibyus@users.noreply.github.com>
2022-01-20 16:53:54 -0300

    Intento fallido de solucionar firebase

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { data } from '../data/data';
+// import { data } from '../data/data';
 import ItemList from './ItemList';
+
+import { collection, getDocs, query, where } from "firebase/firestore";
+import db from '../firebase/firebase';
 
 const ItemListContainer = () => {
   const [loading, setLoading] = useState(true);
@@ -9,25 +12,49 @@ const ItemListContainer = () => {
   const { cat } = useParams();
 
   useEffect(() => {
-  const loadItems = async () => {
-    const promiseItem = await new Promise((res, rej) => {
-      setLoading(true);
-      setTimeout(() => {
-        const Items = cat ? data.filter((item) => item.cat === cat) : data;
-        res(Items)
-    }, 2 * 1000);
-  });
+  async function fetchData(){
+  setLoading(true);
+  const itemsCollection = cat ?
+    query(collection(db, "items"), where('cat', '==', cat))
+    :  
+    collection(db, "items");
 
-  return promiseItem;
-  };
+    const querySnapshot = await getDocs(itemsCollection);
+    console.log(querySnapshot.docs);
+    setItems(querySnapshot.docs.maps((item) => {return { ...item.data(), id: item.id }}))
 
-    const getItems = async () => {
-      const result = await loadItems();
-      setLoading(false);
-      setItems(result);
-    };
-    getItems();
-  }, [cat]);
+    // try {
+    //   const querySnapshot = await getDocs(itemsCollection)
+    //   console.log(querySnapshot.docs)
+    //   setItems(querySnapshot.docs.maps((item) => {return { ...item.data(), id: item.id }
+    // }))
+    // }
+    // catch {
+    //   console.log("Trono como diria el profe")
+    // }
+    setLoading(false);
+  }
+    fetchData();
+    }, [cat]);
+
+  // const loadItems = async () => {
+  //   const promiseItem = await new Promise((res, rej) => {
+  //     setLoading(true);
+  //     setTimeout(() => {
+  //       const Items = cat ? data.filter((item) => item.cat === cat) : data;
+  //       res(Items)
+  //   }, 2 * 1000);
+  // });
+
+  // return promiseItem;
+  // };
+
+  //   const getItems = async () => {
+  //     const result = await loadItems();
+  //     setLoading(false);
+  //     setItems(result);
+  //   };
+  //   getItems();
 
 return (
   <React.Fragment>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,24 +1,33 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
 //firebaseconfig
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APIID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
+  apiKey: "AIzaSyC5alOpNcsZ9qxmu5kTrUwsr08qjcmb5Bc",
+  authDomain: "shop-of-roll.firebaseapp.com",
+  projectId: "shop-of-roll",
+  storageBucket: "shop-of-roll.appspot.com",
+  messagingSenderId: "470904819706",
+  appId: "1:470904819706:web:2398fa79c4b66ebabede43",
+  measurementId: "G-BLED7EHRP3"
 };
+
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APIID,
+//   measurementId: process.env.REACT_APP_MEASUREMENTID
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 export const storage = getStorage(app);
-
 export default db;
